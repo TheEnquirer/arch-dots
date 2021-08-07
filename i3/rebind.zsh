af80a9c1335348151be2c49e4266c60e92d962b4
Huxley Marvit <huxmarv@nuevaschool.org>
2021-08-07 23:22:58 +0000

    rebindss

---
+++ i3/rebind.zsh
@@ -0,0 +1,11 @@
+#jjj
+echo "yeeeet"
+
+xset r rate 170 40
+setxkbmap -option ctrl:swapcaps
+
+xmodmap -e "keycode 191 = parenleft parenleft parenleft"
+xmodmap -e "keycode 192 = parenright parenright parenright"
+
+xmodmap -e "keycode 193 = braceleft braceleft braceleft"
+xmodmap -e "keycode 194 = braceright braceright braceright"
