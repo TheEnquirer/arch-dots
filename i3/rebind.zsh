460ab4ed326d1344e0d12339d547faae1356c519
Huxley Marvit <huxmarv@nuevaschool.org>
2021-08-08 14:54:06 +0000

    ACTULLY use it as a seperate mod lets GO

--- i3/rebind.zsh
+++ i3/rebind.zsh
@@ -6,3 +6,15 @@ xmodmap -e "keycode 192 = parenright parenright parenright"
 
 xmodmap -e "keycode 193 = braceleft braceleft braceleft"
 xmodmap -e "keycode 194 = braceright braceright braceright"
+
+
+#spare_modifier="Hyper_L"
+#xmodmap -e "keycode 134 = $spare_modifier"
+#xmodmap -e "remove mod4 = $spare_modifier"
+#xmodmap -e "add ControlR = $spare_modifier"
+xmodmap -e 'clear mod3'
+xmodmap -e 'keycode 134 = Hyper_R'
+xmodmap -e 'remove mod4 = Hyper_R'
+xmodmap -e 'add mod3 = Hyper_R'
+
+
